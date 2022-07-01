9fc9a56a1b20af44121543b3212573c9b5e23e75
castroolympias <blackcarrera@msn.com>
2022-06-30 21:33:27 -0600

    This is now using an Apollo Server and react is still able to use the REST API data, next is integrating GraphQl to make queries against the same database that the REST API can use

--- server/server.js
+++ server/server.js
@@ -1,23 +1,65 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const path = require('path');
+// const app = express();
+// const db = require('./configuration/Database');
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(require('./routes'));
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`🌍 API server running on port ${PORT}!`);
+//     console.log(`🌍 Use GraphQL at http://localhost:${PORT}`);
+//   });
+// });
+
 const express = require('express');
-const mongoose = require('mongoose');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const app = express();
+const {typeDefs, resolvers} = require('./connections')
+const { authMiddleware } = require('./configuration/Authentication');
 const db = require('./configuration/Database');
+// Once you have concruently installed npm start at root, with your react client side app installed, change the port to 3001, otherwise, for backend only, using Apollo Studio, use 3000
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+async function startApolloServer() {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+      });
+      await server.start();
+      const app = express();
+      server.applyMiddleware({ app });
+      
+      app.use(express.urlencoded({ extended: false }));
+      app.use(express.json());
+ 
+      if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+      }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+      // While this is fine with other full stack MERN apps, this prevents react from consuming REST API data for my hybrid MERNSTACK using REST APIs
+      // app.get('*', (req, res) => {
+      //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      // })
+      
+      app.use(require('./routes'));
+
+      db.once('open', () => {
+        app.listen(PORT, () => {
+          console.log(`🌍 API server running on port ${PORT}!`);
+          console.log(`🌍 Use GraphQL at http://localhost:${PORT}`);
+        });
+      });
 }
-
-app.use(require('./routes'));
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 API server running on port ${PORT}!`);
-    console.log(`🌍 Use GraphQL at http://localhost:${PORT}`);
-  });
-});
+startApolloServer();
