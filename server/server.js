118645e88ef55824ea9184152a321ac478f29257
castroolympias <blackcarrera@msn.com>
2022-06-30 20:59:22 -0600

    current server.js code works with concurrently and allowing react to fetch data from the REST API

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,8 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// You can run this server in Concurrently using react with a REST API
+// this code here app.get causes an error not allowing to fetch data from the REST API
+// SyntaxError: Unexpected token < in JSON at position 0
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/public/index.html'));
+// })
+
+
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
